30d3434675a9eedd10f7d1848553a5669a2cd10b
Collins Kiprono Rotich <crotich@safaricom.co.ke>
2024-02-28 12:39:46 +0300

    Fixed team mtandao heading

--- app/page.js
+++ app/page.js
@@ -5,18 +5,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="flex items-center justify-center pt-8">
-        <div className="flex items-center">
+        <div className="flex">
           <Image
             src={"/favicon.ico"}
-            width={100}
+            width={60}
             height={40}
             alt="safaricom logo"
           />
-          <h1 className="text-4xl">RF-Audit Team Mtandao Hackalympics 2024</h1>
+          <h1 className="text-4xl">RF-Audit Team Mtandao- Hackalympics 2024</h1>
         </div>
-        <div></div> {/* This is a placeholder for the right side */}
-      </div>
 
       
       <div className="pt-8 px-20">
